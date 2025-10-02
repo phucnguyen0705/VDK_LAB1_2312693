4a534795419bfea0480960dd7936d6c56064b28b
phucnguyen0705 <phuc.nguyen0705@hcmut.edu.vn>
2025-10-02 14:44:37 +0700

    update l1e1

--- l1e1/Core/Src/main.c
+++ l1e1/Core/Src/main.c
@@ -63,43 +63,21 @@ static void MX_GPIO_Init(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+  HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, RESET);
+    HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, SET);
   while (1)
   {HAL_Delay(2000);
   HAL_GPIO_TogglePin(a_GPIO_Port, a_Pin);
   HAL_GPIO_TogglePin(b_GPIO_Port, b_Pin);
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
   }
-  /* USER CODE END 3 */
+
 }
 
 /**
